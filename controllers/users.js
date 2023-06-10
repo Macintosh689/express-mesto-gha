7c75b94571c898a26b91a6d068f0d2fee7f4c062
Macintosh689 <macintosh689@gmail.com>
2023-06-10 14:16:01 +0300

    отправка на первое ревью  13 пр

--- controllers/users.js
+++ controllers/users.js
@@ -45,7 +45,7 @@ module.exports.getUserById = (req, res) => {
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
   const { _id } = req.user;
-  User.findByIdAndUpdate(_id, { name, about }, { new: true })
+  User.findByIdAndUpdate(_id, { name, about }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
         return res.status(404).send({ message: 'пользователь не найден.' });
@@ -63,7 +63,7 @@ module.exports.updateProfile = (req, res) => {
 module.exports.updateAvatar = (req, res) => {
   const avatar = req.body;
   const { _id } = req.user;
-  User.findByIdAndUpdate(_id, avatar, { new: true })
+  User.findByIdAndUpdate(_id, avatar, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
         return res.status(404).send({ message: 'пользователь не найден.' });
