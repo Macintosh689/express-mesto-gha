204bb3bb3e589222f7482507db222af8deabdd9d
Macintosh689 <macintosh689@gmail.com>
2023-06-21 15:35:43 +0300

    отправка на первое ревью пр14

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,13 @@ const {
 const { REGEX } = require('../middlewares/validation');
 
 userRouter.get('/', getUsers);
-
+userRouter.get('/me', getCurrentUser);
 userRouter.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().required().length(24).hex(),
   })
 }), getUserById);
-userRouter.get('/me', getCurrentUser);
+
 userRouter.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
